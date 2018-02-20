909be4a00fdfe7ab8aab70b58bf2dfa8baa76eca
YukunVVan <31962749+YukunVVan@users.noreply.github.com>
2018-02-20 16:28:32 -0500

    add required package

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='geo_street_talk',
-      version='0.1',
+      version='0.1.2',
       description='Geo-street-talk',
-      url='https://github.com/YukunVVan/geo-street-talk/',
+      url='https://github.com/Streets-Data-Collaborative/geo-street-talk-global',
       author='Yukun Wan',
       author_email='yw3447@nyu.edu',
       license='MIT',
       packages=['geo_street_talk'],
+      install_requires=['osmnx','geopandas','shapely'],
       zip_safe=False)
